a3bacbe1c30386bafa7d8f40b2fa39107eb7ec2f
Saina <chimsaina98@gmail.com>
2025-07-10 16:49:38 +0700

    metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,25 @@
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+  title: {
+    template: "%s | This is About page",
+    default: "About Page",
+  },
+  openGraph: {
+    title: "About page",
+    description: "About page",
+    images: [
+      {
+        url: "https://i.pinimg.com/736x/da/02/58/da0258f4c1fb543aa5d76336e3255242.jpg",
+        width: 800,
+        height: 650,
+        alt: "Car",
+        type: "image/png",
+      },
+    ],
+  },
+};
 const ProductPage = async () => {
   return (
     <div>
